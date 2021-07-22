18e7a7765bfcd5ead93d648694ad178854de2c29
odmar <odmar.goyvaerts@gmail.com>
2021-07-22 17:44:43 +0200

    fixed bug where some sequences would output the wrong function

--- lua/cpp/utils.lua
+++ lua/cpp/utils.lua
@@ -1,5 +1,19 @@
 local M = {}
 
+local patterns = {
+    " (%w+)%(",
+    " (%w+) %(", 
+    " (%w+)_(%w+)%(",
+    " (%w+)_(%w+) %(",
+    " (%w+)%g%(",
+    " (%w+)%g %(",
+    " (%w+)%g%g%(",
+    " (%w+)%g%g %(",
+    " (%w+)%g%g%g%(",
+    " (%w+)%g%g%g %(" 
+}
+
+-- split a string into a table
 local function split(s, delimiter)
     result = {};
     for match in (s..delimiter):gmatch("(.-)"..delimiter) do
@@ -8,27 +22,26 @@ local function split(s, delimiter)
     return result;
 end
 
+-- make the path shorter to where only the name of the file remains
 function M.path_shortening(s)
     b,c = string.find(s,"(%w+)%.")
     s = string.sub(s,b,c - 1)
     return s
 end
 
+-- change function prototype to normal function
 function M.change_func(s, class)
-    b,c = string.find(s," (%w+)%(")
-    if b == nil then
-        b,c = string.find(s," (%w+) %(")
-    end
-    if b == nil then
-        b,c = string.find(s," (%w+)_(%w+)%(")
-    end
-    if b == nil then
-        b,c = string.find(s," (%w+)_(%w+) %(")
+    for k,v in ipairs(patterns) do
+        b,c = string.find(s,v)
+        if b ~= nil then
+            break
+        end
     end
     s =  {string.sub(s,5,b) .. class .. "::" .. string.sub(s,b + 1,#s - 1) .. "{","\t","}"}
     return s
 end
 
+-- search files in file structure and return true if it is found
 local function file_exists(buffer)
     local find = io.popen("find ./ -name '*.cpp'")
     local result = split(find:read("*a"), "\n")
@@ -40,6 +53,7 @@ local function file_exists(buffer)
     return false, nil
 end
 
+-- write to file 
 function M.file_write(buffer,input, index)
     local k,id = file_exists(buffer)
     if k then
