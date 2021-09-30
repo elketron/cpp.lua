bbba89cb407d061952c4fb7ab7b2bb8f05fd85e7
odmar <odmar.goyvaerts@gmail.com>
2021-09-30 20:29:17 +0200

    added arduino compile method

---
+++ lua/funcs/arduino.lua
@@ -0,0 +1,33 @@
+-- helper functions for arduino
+-- compile function
+-- upload function
+-- serial terminal functionality
+local utils = require"funcs.utils"
+
+local M = {}
+
+M.compile_options = {fqbn = "arduino:avr:nano"}
+M.upload_options = {
+    fqbn = "arduino:avr:nano:cpu=atmega328Bold",
+    port = ""
+}
+
+local function get_port()
+    
+end
+
+function M.arduino_compile()
+    file_name = utils.path_shortening(vim.api.nvim_buf_get_name(0))
+    local output = vim.api.nvim_exec(":! arduino-cli compile --fqbn "..M.compile_options.fqbn.. " ".. file_name,true)
+    print(output)
+end
+
+function M.arduino_upload()
+   file_name = utils.path_shortening(vim.api.nvim_buf_get_name(0))
+   M.upload_options.port = get_port()
+   local output = vim.api.nvim_exec(":! arduino-cli upload -p ".. upload_options.port..  " --fqbn ".. upload_options.fqbn .. " ".. file_name)
+   print(output)
+end
+
+return M
+
--- lua/funcs/init.lua
+++ lua/funcs/init.lua
@@ -1,10 +1,12 @@
 local mkfun = require("funcs.cpp")
 local comment = require("funcs.comment")
+local ard = require("funcs.arduino")
 local M = {}
 
 M.mkfun = mkfun.mkfun
 M.com = comment.comment
 M.unc = comment.uncomment
+M.acom = ard.arduino_compile
 
 return M
 
